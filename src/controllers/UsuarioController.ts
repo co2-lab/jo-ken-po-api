857d1294b202a33cd3b20b9d51d0631f4a9be372
gcodeco <codeco.gcs@gmail.com>
2024-07-01 19:05:21 -0300

    refactor(custom): adicionado  método para verificar se o usuário já existe e atualizar mensagens

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,3 +1,4 @@
+import { Messages } from './../messagens'
 import { Request, Response } from 'express'
 import { usuarioRepository } from '../repositories/usuarioRepository'
 import { Usuario } from '../entities/Usuario'
@@ -5,7 +6,8 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt' // Adicionar bcrypt para hash de senha
 
 export class UsuarioController {
-  async create(req: Request, res: Response): Promise<void> {
+  async create (req: Request, res: Response): Promise<Response> {
+    // eslint-disable-next-line camelcase
     const { name, date_nasc, cpf, rg, username, password, email } =
       req.body as {
         name: string
@@ -18,6 +20,11 @@ export class UsuarioController {
       }
 
     try {
+      // Verifica se o usuário já existe
+      const usuarioExistente = await this.verificarUsuarioExistente(email, username)
+      if (usuarioExistente) {
+        return res.status(422).json({ message: Messages.USER_ALREADY_EXISTS })
+      }
       const hashedPassword = await bcrypt.hash(password, 10) // Gerar hash da senha
       const cpfNumber = parseFloat(cpf.replace(/\D/g, ''))
       const formattedDataNasc = new Date(date_nasc)
@@ -29,52 +36,64 @@ export class UsuarioController {
         rg,
         username,
         hashedPassword,
-        email,
+        email
       )
 
       usuario.moeda = 1000
 
       const savedUsuario = await usuarioRepository.save(usuario)
       // Remove a senha do objeto antes de enviar a resposta
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password: omitPassword, ...responseUsuario } = savedUsuario
-      res.status(201).json(responseUsuario)
+      return res.status(201).json(responseUsuario)
     } catch (error) {
       console.error('Erro ao criar usuário:', error)
-      res.status(500).json({ message: 'Internal Server Error' })
+      return res.status(500).json({ message: Messages.INTERNAL_SERVER_ERROR })
     }
   }
 
-  async autenticar(req: Request, res: Response): Promise<Response> {
+  async autenticar (req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body as { email: string; password: string }
     const secret = 'teste123'
 
     try {
       if (!email || !password) {
-        return res.status(422).json({ msg: 'Email e senha são obrigatórios' })
+        return res.status(422).json({ message: Messages.EMAIL_AND_PASSWORD_REQUIRED })
       }
 
       const usuario = await findUsuario(email)
       if (!usuario) {
-        return res.status(404).json({ msg: 'Usuário não encontrado' })
+        return res.status(404).json({ message: Messages.NO_USERS_FOUND })
       }
 
       const passwordMatch = await bcrypt.compare(password, usuario.password) // Comparar hashes de senha
       if (!passwordMatch) {
-        return res.status(422).json({ msg: 'Senha incorreta' })
+        return res.status(422).json({ message: Messages.INCORRECT_PASSWORD })
       }
 
       const token = jwt.sign({ id: usuario.id }, secret)
       return res
         .status(200)
-        .json({ msg: 'Autenticação realizada com sucesso', token })
+        .json({ msg: Messages.AUTHENTICATION_SUCCESS, token })
     } catch (error) {
       console.error('Erro ao autenticar usuário:', error)
-      return res.status(500).json({ msg: 'Internal Server Error' })
+      return res.status(500).json({ message: Messages.INTERNAL_SERVER_ERROR })
+    }
+  }
+
+  async verificarUsuarioExistente (email: string, username: string): Promise<boolean> {
+    try {
+      const usuarioPorEmail = await usuarioRepository.findOne({ where: { email } })
+      const usuarioPorUsername = await usuarioRepository.findOne({ where: { username } })
+      return usuarioPorEmail !== null || usuarioPorUsername !== null
+    } catch (error) {
+      console.error('Erro ao verificar se o usuário já existe:', error)
+      return false
     }
   }
 }
 
-async function findUsuario(email: string): Promise<Usuario | null> {
+async function findUsuario (email: string): Promise<Usuario | null> {
   try {
     return await usuarioRepository
       .createQueryBuilder('usuarios')
--- src/messagens.ts
+++ src/messagens.ts
@@ -8,5 +8,12 @@ export enum Messages {
   NO_USERS_FOUND = 'No users found',
   NO_WINNERS_FOUND = 'No winners found',
   // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
-  BET_NOT_MATCH = 'The bet amount does not match the original value'
+  BET_NOT_MATCH = 'The bet amount does not match the original value',
+  USER_CREATED = 'User created',
+  EMAIL_AND_PASSWORD_REQUIRED = 'Email and password are required',
+  INCORRECT_PASSWORD = 'Incorrect password',
+  AUTHENTICATION_SUCCESS = 'Authentication successful',
+  EMAIL_NOT_PROVIDED = 'Email is required',
+  PASSWORD_NOT_PROVIDED = 'Password is required',
+  USER_ALREADY_EXISTS = 'User already exists'
 }
